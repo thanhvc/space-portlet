cc028b12594464ad84b6435d7c8d3990963f716c
Vi Quoc Hanh <hanh.vi@exoplatform.com>
2012-08-16 15:39:33 +0000

    [Space Activity] Fix bug to make application run on juzu 0.6.0 SNAPSHOT and PLF-4.0

--- src/main/java/org/social/portlet/package-info.java
+++ src/main/java/org/social/portlet/package-info.java
@@ -1,6 +1,5 @@
-@Application(
-    name = "SpaceApplication",
-    defaultController = org.social.portlet.Controller.class)
+@Application
+@Portlet
 @Bindings({
   @Binding(value = SpaceService.class, implementation = GateInMetaProvider.class),
   @Binding(value = IdentityManager.class, implementation = GateInMetaProvider.class),
@@ -8,13 +7,13 @@
 })
 @Assets(
   scripts = {
-    @Script(src = "js/jquery-1.7.1.js"),
-    @Script(src = "js/less-1.2.2.min.js"),
-    @Script(src = "js/bootstrap.js"),
-    @Script(src = "js/bootstrap-collapse.js"),
-    @Script(src = "js/bootstrap-tooltip.js"),
-    @Script(src = "js/bootstrap-popover.js"),
-    @Script(src = "js/space.js")
+    @Script(id = "jquery", src = "js/jquery-1.7.1.min.js"),
+    @Script(src = "js/less-1.2.2.min.js", depends = "jquery"),
+    @Script(src = "js/bootstrap.js", depends = "jquery"),
+    @Script(src = "js/bootstrap-collapse.js", depends = "jquery"),
+    @Script(src = "js/bootstrap-tooltip.js", depends = "jquery"),
+    @Script(src = "js/bootstrap-popover.js", depends = "jquery"),
+    @Script(src = "js/space.js", depends = "juzu.ajax")
   },
   stylesheets = {
     @Stylesheet(src = "css/gatein.less")
@@ -22,15 +21,15 @@
 )
 package org.social.portlet;
 
+import org.social.portlet.providers.GateInMetaProvider;
+import org.social.portlet.providers.IdentityProvider;
 import org.exoplatform.social.core.identity.model.Identity;
-import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.core.space.spi.SpaceService;
+import org.exoplatform.social.core.manager.IdentityManager;
 import juzu.Application;
-import juzu.plugin.binding.Binding;
-import juzu.plugin.binding.Bindings;
 import juzu.plugin.asset.Assets;
 import juzu.plugin.asset.Script;
 import juzu.plugin.asset.Stylesheet;
-import org.social.portlet.providers.GateInMetaProvider;
-import org.social.portlet.providers.IdentityProvider;
-
+import juzu.plugin.binding.Binding;
+import juzu.plugin.binding.Bindings;
+import juzu.plugin.portlet.Portlet;
